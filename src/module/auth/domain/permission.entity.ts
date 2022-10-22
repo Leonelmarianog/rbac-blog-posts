89c4ca1c0f6d881fb7af81bc4b97277e30d4761e
Leonel Gauna <leonel.mariano.g@gmail.com>
2022-10-22 17:56:45 -0300

    Update domain models

--- src/module/auth/domain/permission.entity.ts
+++ src/module/auth/domain/permission.entity.ts
@@ -1,11 +1,8 @@
 import { BaseEntity } from '../../../common/domain/base.entity';
 import { AuthAction } from './auth-actions.enum';
-import { Role } from './role.entity';
 
 export class Permission extends BaseEntity {
   action: AuthAction;
 
   subjectId: number;
-
-  role: Role[];
 }
--- src/module/auth/domain/role.entity.ts
+++ src/module/auth/domain/role.entity.ts
@@ -1,9 +1,6 @@
 import { BaseEntity } from '../../../common/domain/base.entity';
-import { Permission } from './permission.entity';
 import { RoleName } from './role-name.enum';
 
 export class Role extends BaseEntity {
   name: RoleName;
-
-  permissions: Permission[];
 }
